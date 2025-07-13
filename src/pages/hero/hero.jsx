f8f5b7e1d2daf06daa182132633b1e1b48d34e21
PaulMarbella <132251348+PaulMarbella@users.noreply.github.com>
2025-07-13 23:34:10 +0800

    -add resume

--- src/pages/hero/hero.jsx
+++ src/pages/hero/hero.jsx
@@ -1,26 +1,44 @@
-
-function Hero(){
-    return(
-            <section className="container-fluid d-flex justify-content-evenly align-items-center hero gap-3" id="hero" > 
-                <div className="left-hero">
-                    <div data-aos="fade-up">
-                    <h1>Hi There! I'm <span className="name">Paul Marbella Jr</span></h1>
-                    <h3>Software Engineer</h3>
-                    <p>"Blending curiosity and code to bring bold ideas to life. Let's make something meaningful."</p>
-                    <div className="d-flex gap-3">
-                        <a id="resume" href="#contacts">View Resume</a>
-                        <a id="contact-me" href="#contacts">Contact Me</a>
-                    </div>
-                    </div>
-                </div>
-                <div className="pic-container">
-                    <div data-aos="fade-left">
-                        <img src={`${import.meta.env.BASE_URL}assets/portfolio-pic.png`} className=" hero-pic border border-4 border-dark rounded shadow " />
-                    </div>
-                </div>
-                
-            </section>
-    )
+function Hero() {
+  return (
+    <section
+      className="container-fluid d-flex justify-content-evenly align-items-center hero gap-3"
+      id="hero"
+    >
+      <div className="left-hero">
+        <div data-aos="fade-up">
+          <h1>
+            Hi There! I'm <span className="name">Paul Marbella Jr</span>
+          </h1>
+          <h3>Software Engineer</h3>
+          <p>
+            "Blending curiosity and code to bring bold ideas to life. Let's make
+            something meaningful."
+          </p>
+          <div className="d-flex gap-3">
+            <a
+              id="resume"
+              href={`${import.meta.env.BASE_URL}assets/Resume.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View Resume
+            </a>
+            <a id="contact-me" href="#contacts">
+              Contact Me
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="pic-container">
+        <div data-aos="fade-left">
+          <img
+            src={`${import.meta.env.BASE_URL}assets/portfolio-pic.png`}
+            className=" hero-pic border border-4 border-dark rounded shadow "
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
